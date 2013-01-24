4f39cef843a7ca83f89b0a7dd3687be48969aa2f
Harsh Gupta <harshg0910@gmail.com>
2013-01-25 00:01:15 +0530

    Client working.TODO - server

--- client/client.c
+++ client/client.c
@@ -9,57 +9,56 @@
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    
-    if (argc < 6) {
-       error("	      
-		       A: Adding an event: ./mycal hostname port myname add 031505 0800 0900 Exam
-		       B: Removing an event: ./mycal hostname port myname remove 031505 0800
-		       C: Updating an event: ./mycal hostname port myname update 031505 0800 1000 OralExam
-		       D: Getting an event 1: ./mycal hostname port myname get 031505 0800
-		       E: Getting an event 2: ./mycal hostname port myname get 031505
-	       ");
-     
-    }
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+	char buffer[255];
+	if (argc < 6) {
+		printf("Adding an event: ./mycal hostname port myname add 031505 0800 0900 Exam\nRemoving an event: ./mycal hostname port myname remove 031505 0800\nUpdating an event: ./mycal hostname port myname update 031505 0800 1000 OralExam\nGetting an event 1: ./mycal hostname port myname get 031505 0800\nGetting an event 2: ./mycal hostname port myname get 031505\n");
+		exit(2);
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR in connecting");
-    
-    //printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+	}
+
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+		error("ERROR opening socket");
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+			(char *)&serv_addr.sin_addr.s_addr,
+			server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+		error("ERROR in connecting");
+	
+	
+	for(int i=3;i<argc;i++){
+		strcat(buffer,argv[i]);
+		strcat(buffer," ");
+	}
+	printf("%s\n",buffer);
+	n = write(sockfd,buffer,strlen(buffer));
+	if (n < 0) 
+		error("ERROR writing to socket");
+	bzero(buffer,256);
+	n = read(sockfd,buffer,255);
+	if (n < 0) 
+		error("ERROR reading from socket");
+	printf("%s\n",buffer);
+	close(sockfd);
+	return 0;
 }
