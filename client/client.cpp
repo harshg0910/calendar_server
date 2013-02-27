7ad6e3f08b5bc6c22eeadda8537d922f02eef57f
Harsh Gupta <harshg0910@gmail.com>
2013-02-27 20:01:59 +0530

    client is done.Almost whole project is done.Do some testing

--- client/client.cpp
+++ client/client.cpp
@@ -1,19 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <unistd.h>
-#include <string.h>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <ctime>
 #include <iostream>
+#include <sstream>
 using namespace std;
+#define BUFFER_LEN 256
 
 void error(const char *msg)
 {
 	perror(msg);
 	exit(1);
+}
+
+
+string to_string(int a){
+	ostringstream convert;   // stream used for the conversion
+	// insert the textual representation of 'Number' in the characters in the stream
+	convert << a;      
+	return convert.str(); // set 'Result' to the contents of the stream
 }
 
 int main(int argc, char *argv[])
@@ -23,11 +33,16 @@ int main(int argc, char *argv[])
 	struct hostent *server;
 
 	char buffer[255];
-	/*if (argc < 6) {
-	  printf("Adding an event: ./mycal hostname port myname add 031505 0800 0900 Exam\nRemoving an event: ./mycal hostname port myname remove 031505 0800\nUpdating an event: ./mycal hostname port myname update 031505 0800 1000 OralExam\nGetting an event 1: ./mycal hostname port myname get 031505 0800\nGetting an event 2: ./mycal hostname port myname get 031505\n");
-	  exit(2);
+	if (argc < 5) {
+		cout << "Adding an event: ./mycal hostname port myname add 031505 0800 0900 Exam\n";
+		cout << "Removing an event: ./mycal hostname port myname remove 031505 0800\n";
+		cout << "Updating an event: ./mycal hostname port myname update 031505 0800 1000 OralExam\n";
+		cout << "Getting an event 1: ./mycal hostname port myname get 031505 0800\n";
+		cout << "Getting an event 2: ./mycal hostname port myname get 031505\n";
+		cout << "Getting All events : ./mycal hostname port myname getall\n";
+		exit(2);
 
-	  }*/
+	}
 
 	portno = atoi(argv[2]);
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -48,26 +63,70 @@ int main(int argc, char *argv[])
 	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
 		error("ERROR in connecting");
 
-	/*
-	   for(int i=3;i<argc;i++){
-	   strcat(buffer,argv[i]);
-	   strcat(buffer," ");
-	   }*/
-	for(int k=0;k<100;k++){
-		sleep(1);
-		//strcpy(buffer,"harsh1 add 1234 1234 2345 t1");
-		cin.getline(buffer,255);
-		printf("%s\n",buffer);
+	if(!strcmp(argv[4],"getall")){
+		bzero(buffer,BUFFER_LEN);
+		strcat(buffer,argv[3]);
+		strcat(buffer," get_number");
+		n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0) 
+			error("ERROR writing to socket");
+		bzero(buffer,BUFFER_LEN);
+		n = read(sockfd,buffer,BUFFER_LEN);
+		if (n < 0) 
+			error("ERROR reading from socket");
+		int number = atoi(buffer);
+		for(int i=1;i<=number;i++){
+			sleep(2);
+			bzero(buffer,BUFFER_LEN);
+			strcat(buffer,argv[3]);
+			strcat(buffer," get_ith ");
+			strcat(buffer,to_string(i).c_str());
+			
+			n = write(sockfd,buffer,strlen(buffer));
+			if (n < 0) 
+				error("ERROR writing to socket");
+
+			bzero(buffer,BUFFER_LEN);
+			n = read(sockfd,buffer,BUFFER_LEN);
+			if (n < 0) 
+				error("ERROR reading from socket");
+			printf("%s",buffer);
+
+		}
+
+	}else{	
+		for(int i=3;i<argc;i++){
+			strcat(buffer,argv[i]);
+			strcat(buffer," ");
+		}
 		n = write(sockfd,buffer,strlen(buffer));
 		if (n < 0) 
 			error("ERROR writing to socket");
 
-		bzero(buffer,256);
-		n = read(sockfd,buffer,255);
+		bzero(buffer,BUFFER_LEN);
+		n = read(sockfd,buffer,BUFFER_LEN);
 		if (n < 0) 
 			error("ERROR reading from socket");
 		printf("%s",buffer);
+
 	}
-	close(sockfd);
-	return 0;
+
+	/*
+	   for(int k=0;k<100;k++){
+	   sleep(1);
+//strcpy(buffer,"harsh1 add 1234 1234 2345 t1");
+cin.getline(buffer,255);
+printf("%s\n",buffer);
+n = write(sockfd,buffer,strlen(buffer));
+if (n < 0) 
+error("ERROR writing to socket");
+
+bzero(buffer,256);
+n = read(sockfd,buffer,255);
+if (n < 0) 
+error("ERROR reading from socket");
+printf("%s",buffer);
+}*/
+close(sockfd);
+return 0;
 }
